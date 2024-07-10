a467479b701bdce9ac54f2f838f212e898b4b9c5
Simon Welz <welz.simon@outlook.de>
2024-07-10 19:57:55 +0200

    Added Email Notification for Jobs

--- job_script.sh
+++ job_script.sh
@@ -8,6 +8,8 @@
 #SBATCH --job-name=knowledge_integration_training
 #SBATCH --output=outputs/output_large_c4_%j.txt
 #SBATCH --error=outputs/error_large_c4_%j.txt
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=welz.simon@outlook.de
 
 module load CUDA/11.8.0
 module load Anaconda3/2022.10
