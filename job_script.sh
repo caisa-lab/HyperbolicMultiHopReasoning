c2f4f33828a26f908c40cbefb1ef37a72844356f
Simon Welz <s6snwelz@uni-bonn.de>
2024-07-10 19:55:45 +0200

    Changed values

--- job_script.sh
+++ job_script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #SBATCH --partition=A100medium
-#SBATCH --time=20:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --gpus=1
 
 #SBATCH --ntasks=1
 #SBATCH --job-name=knowledge_integration_training
-#SBATCH --output=outputs/output_%j.txt
-#SBATCH --error=outputs/error_%j.txt
+#SBATCH --output=outputs/output_large_c4_%j.txt
+#SBATCH --error=outputs/error_large_c4_%j.txt
 
 module load CUDA/11.8.0
 module load Anaconda3/2022.10
@@ -18,4 +18,4 @@ pip install transformers --quiet
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu118 --quiet
 pip install sentencepiece
 
-python -u train_knowledge_integration.py
+python -u train_knowledge_integration.py --c4
