f750833f4671604cbbaefdfc80a6248f250fcb0a
Simon Welz <welz.simon@outlook.de>
2024-08-05 17:35:41 +0200

    Fixed indent

--- src/models/hyperbolic_prompt_learning_model.py
+++ src/models/hyperbolic_prompt_learning_model.py
@@ -64,8 +64,8 @@ class HyperbolicSoftPromptModel(nn.Module):
                  with_model_state_dict = True,
                  curvature : float = 1.0):
         super(HyperbolicSoftPromptModel, self).__init__()
-	if hyperbolic_knit5_checkpoint_path is not None:
-            self.knit5 = load_model_checkpoint(hyperbolic_knit5, hyperbolic_knit5_checkpoint_path, with_model_state_dic$
+        if hyperbolic_knit5_checkpoint_path is not None:
+            self.knit5 = load_model_checkpoint(hyperbolic_knit5, hyperbolic_knit5_checkpoint_path, with_model_state_dict=with_model_state_dict)
         if isinstance(hyperbolic_knit5, HyperbolicT5Model):
             self.knit5 : T5Model = self.knit5.t5
             print("HyperbolicT5")
