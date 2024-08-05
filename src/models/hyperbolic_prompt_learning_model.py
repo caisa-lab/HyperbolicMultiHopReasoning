Fixed Gradient Issue, Added temp HyperbolicT5Mapping model

src/models/hyperbolic_prompt_learning_model.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 from utils.util import get_top_token_embeddings, expmap0, logmap0
 from utils.trainer_utils import load_model_checkpoint
-from .hyperbolic_t5_model import HyperbolicT5Model
+from .hyperbolic_t5_model import HyperbolicT5Model, HyperbolicT5MapEmbeddings
 from transformers import AutoTokenizer, T5ForConditionalGeneration, T5Model
 from src.config import Config
 from .hyperbolic_model_utils import HyperbolicSoftPrompts
@@ -57,21 +57,17 @@ for input_str, label in training_data:
 """
 class HyperbolicSoftPromptModel(nn.Module):
     def __init__(self,
-                 hyperbolic_knit5 : Union[HyperbolicT5Model, T5ForConditionalGeneration],
+                 hyperbolic_knit5 : Union[HyperbolicT5MapEmbeddings, T5ForConditionalGeneration, HyperbolicT5Model],
                  hyperbolic_knit5_checkpoint_path : str,
                  model_name : str,
                  hyperbolic_soft_prompt : HyperbolicSoftPrompts = None,
                  with_model_state_dict = True,
                  curvature : float = 1.0):
         super(HyperbolicSoftPromptModel, self).__init__()
+        self.knit5 = hyperbolic_knit5
         if hyperbolic_knit5_checkpoint_path is not None:
-            self.knit5 = load_model_checkpoint(hyperbolic_knit5, hyperbolic_knit5_checkpoint_path, with_model_state_dict=with_model_state_dict)
-        if isinstance(hyperbolic_knit5, HyperbolicT5Model):
-            self.knit5 = self.knit5.t5
-            print("HyperbolicT5")
-        elif isinstance(hyperbolic_knit5, T5ForConditionalGeneration):
-            self.knit5 : T5Model = hyperbolic_knit5
-            print("T5")
+            self.knit5 = load_model_checkpoint(self.knit5, hyperbolic_knit5_checkpoint_path, with_model_state_dict=with_model_state_dict)
+
             
             
         self.curvature = curvature
@@ -105,7 +101,7 @@ class HyperbolicSoftPromptModel(nn.Module):
     
     
     def forward(self, inputs, labels):
-        hyperbolic_soft_prompt_input = self.hyperbolic_soft_prompt.weight.data.expand(inputs['input_ids'].size(0), -1, -1).to(self.device)
+        hyperbolic_soft_prompt_input = self.hyperbolic_soft_prompt.weight.expand(inputs['input_ids'].size(0), -1, -1).to(self.device)
         input_embeddings = self.knit5.shared(inputs['input_ids'])  # Convert input IDs to embeddings
 
         concatenated_embeddings = torch.cat([hyperbolic_soft_prompt_input, input_embeddings], dim=1)
@@ -115,25 +111,24 @@ class HyperbolicSoftPromptModel(nn.Module):
         #Adjust attention mask (take all of the soft prompt tokens should be attented)
         soft_prompt_attention_mask = torch.ones((inputs['attention_mask'].size(0), hyperbolic_soft_prompt_input.size(1)), device=self.device)
         concatenated_attention_mask = torch.cat((soft_prompt_attention_mask, inputs['attention_mask']), dim=1)
+        
+        
         outputs = self.knit5(inputs_embeds=hyperbolic_concat_embedding, attention_mask=concatenated_attention_mask, labels=labels)
         return outputs
-    
+    """
     def generate(self, inputs, max_length=50, num_beams = 5, early_stopping=True):
         soft_prompt_input = self.hyperbolic_soft_prompt.soft_prompts.weight.unsqueeze(0).expand(inputs['input_ids'].size(0), -1, -1).to(self.device)
         input_embeddings = self.knit5.shared(inputs['input_ids'])  # Convert input IDs to embeddings
 
-        hyperbolic_input_embeddings = expmap0(input_embeddings, self.curvature)
-        
-        hyperbolic_soft_prompt_embeddings = expmap0(soft_prompt_input, self.curvature)
-        
-
         concatenated_embeddings = torch.cat([hyperbolic_soft_prompt_embeddings, hyperbolic_input_embeddings], dim=1)
+        
+        hyperbolic_concat_embedding = expmap0(concatenated_embeddings, self.curvature)
         
         #Adjust attention mask (take all of the soft prompt tokens should be attented)
         pp_attention_mask = torch.ones((inputs['attention_mask'].size(0), soft_prompt_input.size(1)), device=self.device)
         concatenated_attention_mask = torch.cat((pp_attention_mask, inputs['attention_mask']), dim=1)
 
-        outputs = self.knit5.generate(inputs_embeds=concatenated_embeddings, attention_mask=concatenated_attention_mask, max_length=max_length, num_beams=num_beams, early_stopping=early_stopping)
+        outputs = self.knit5.generate(inputs_embeds=hyperbolic_concat_embedding, attention_mask=concatenated_attention_mask, max_length=max_length, num_beams=num_beams, early_stopping=early_stopping)
         return outputs
     
-    
+    """

src/models/hyperbolic_t5_model.py
@@ -1,48 +1,139 @@
-import torch
-import torch.nn as nn
 from utils.util import expmap0, logmap0
-from transformers import T5Model
-from transformers import AutoTokenizer
-from src.config import Config
+from utils.trainer_utils import load_model_checkpoint
+from transformers import T5ForConditionalGeneration, T5Config
+from .hyperbolic_model_utils import HyperbolicEmbedding
+from torch import Tensor, FloatTensor, LongTensor, BoolTensor
+from typing import Tuple
 
 
-#TODO Implement Hyperbolic encoder with hyperbolic embedding, attention, feedforward ...
-#TODO Convert the output to euclidean and input it into the decoder
-class HyperbolicT5Model(nn.Module):
+class HyperbolicT5MapEmbeddings(T5ForConditionalGeneration):
     def __init__(self,
-                 t5 : T5Model,
-                 model_name : str,
+                 model_name : str = 'google/t5-large-lm-adapt',
                  curvature : float = 1.0):
-        super(HyperbolicT5Model, self).__init__()
-        self.t5 = t5
+        super(HyperbolicT5MapEmbeddings, self).__init__(T5Config.from_pretrained(model_name))
+        print(f"Loaded model: {model_name}")
         self.curvature = curvature
         self.model_name = model_name
-        self.config = Config()
-
+        #if checkpoint_path:
+         #   load_model_checkpoint(self.t5, checkpoint_path, with_model_state_dict=True)
+            
     
-    
-    def forward(self, input_ids, attention_mask, labels, hyperbolic = True):
-        input_embeddings = self.t5.shared(input_ids)  # Convert input IDs to embeddings
-        
-        if hyperbolic:
+    def forward(self, input_ids: LongTensor = None,
+            attention_mask: FloatTensor = None,
+            decoder_input_ids: LongTensor = None,
+            decoder_attention_mask: BoolTensor = None,
+            head_mask: FloatTensor = None,
+            decoder_head_mask: FloatTensor = None,
+            cross_attn_head_mask: Tensor = None,
+            encoder_outputs: Tuple[Tuple[FloatTensor]] = None,
+            past_key_values: Tuple[Tuple[FloatTensor]] = None,
+            inputs_embeds: Tensor = None,
+            decoder_inputs_embeds: Tensor = None,
+            use_cache: bool = None,
+            output_attentions: bool = None,
+            output_hidden_states: bool = None,
+            return_dict: bool = None,
+            labels = None):
+        if inputs_embeds is None:
+            input_embeddings = self.shared(input_ids)  # Convert input IDs to embeddings
+            
             #Map input embeddings to hyperbolic space
             input_embeddings = expmap0(input_embeddings, self.curvature)
-            
-        encoder_outputs = self.t5.encoder(inputs_embeds = input_embeddings, attention_mask=attention_mask)
-        encoder_hidden_states = encoder_outputs.last_hidden_state
-        if hyperbolic:
-            encoder_hidden_states = logmap0(encoder_hidden_states, self.curvature)
-            
-        decoder_outputs = self.t5.decoder(encoder_hidden_states=encoder_hidden_states, attention_mask=attention_mask, labels=labels)
-
-        return decoder_outputs
+        else:
+            input_embeddings = inputs_embeds
+        return super(HyperbolicT5MapEmbeddings, self).forward(input_ids=input_ids,
+                                                            attention_mask=attention_mask,
+                                                            decoder_input_ids=decoder_input_ids,
+                                                            decoder_attention_mask=decoder_attention_mask,
+                                                            head_mask=head_mask,
+                                                            decoder_head_mask=decoder_head_mask,
+                                                            cross_attn_head_mask=cross_attn_head_mask,
+                                                            encoder_outputs=encoder_outputs,
+                                                            past_key_values=past_key_values,
+                                                            decoder_inputs_embeds=decoder_inputs_embeds,
+                                                            use_cache=use_cache,
+                                                            output_attentions=output_attentions,
+                                                            output_hidden_states=output_hidden_states,
+                                                            return_dict=return_dict,
+                                                            inputs_embeds=input_embeddings,  # pass inputs_embeds here
+                                                            labels=labels)
+    
+    
     
     def generate(self, input_ids, attention_mask, max_length=50, num_beams = 5, early_stopping=True):
-        input_embeddings = self.t5.shared(input_ids)  # Convert input IDs to embeddings
+        input_embeddings = self.shared(input_ids)  # Convert input IDs to embeddings
 
         #Map input embeddings to hyperbolic space
         hyperbolic_input_embeddings = expmap0(input_embeddings, self.curvature)
 
-        outputs = self.t5.generate(inputs_embeds=hyperbolic_input_embeddings, attention_mask=attention_mask, max_length=max_length, num_beams=num_beams, early_stopping=early_stopping)
+        outputs = self.generate(inputs_embeds=hyperbolic_input_embeddings, attention_mask=attention_mask, max_length=max_length, num_beams=num_beams, early_stopping=early_stopping)
+        return outputs
+
+class HyperbolicT5Model(T5ForConditionalGeneration):
+    """
+    For now the shared layer is replaced with an hyerbolic Embedding layer which is then trained. 
+    We feed the input_ids through the hyperbolic embedding layer and convert them to the euclidean space and push it through the original t5 model which works in euclidean space.
+    TODO We can also try to turn additional layers also in the hyperbolic space...
+    """
+    def __init__(self,
+                 model_name : str = 'google/t5-large-lm-adapt',
+                 curvature : float = 1.0):
+        config = T5Config.from_pretrained(model_name)
+        super(HyperbolicT5Model, self).__init__(config)
+        self.curvature = curvature
+        self.model_name = model_name
+        num_embeddings = self.shared.num_embeddings
+        embedding_dim = self.shared.embedding_dim
+        self.encoder_embeddings = HyperbolicEmbedding(num_embeddings, embedding_dim, curvature = curvature) #TODO Initialize this with the exponential mapped weights of the shared layers of T5 
+
+    
+    
+    def forward(self,
+                input_ids: LongTensor = None,
+    attention_mask: FloatTensor = None,
+    decoder_input_ids: LongTensor = None,
+    decoder_attention_mask: BoolTensor = None,
+    head_mask: FloatTensor = None,
+    decoder_head_mask: FloatTensor = None,
+    cross_attn_head_mask: Tensor = None,
+    encoder_outputs: Tuple[Tuple[FloatTensor]] = None,
+    past_key_values: Tuple[Tuple[FloatTensor]] = None,
+    inputs_embeds: Tensor = None,
+    decoder_inputs_embeds: Tensor = None,
+    use_cache: bool = None,
+    output_attentions: bool = None,
+    output_hidden_states: bool = None,
+    return_dict: bool = None,
+    labels = None):
+        input_embeddings = self.encoder_embeddings(input_ids)  # Convert input IDs to hyperbolic embeddings
+        
+        input_embeddings = logmap0(input_embeddings, c = self.curvature)
+            
+
+
+        return super(HyperbolicT5Model, self).forward(input_ids=input_ids,
+    attention_mask=attention_mask,
+    decoder_input_ids=decoder_input_ids,
+    decoder_attention_mask=decoder_attention_mask,
+    head_mask=head_mask,
+    decoder_head_mask=decoder_head_mask,
+    cross_attn_head_mask=cross_attn_head_mask,
+    encoder_outputs=encoder_outputs,
+    past_key_values=past_key_values,
+    decoder_inputs_embeds=decoder_inputs_embeds,
+    use_cache=use_cache,
+    output_attentions=output_attentions,
+    output_hidden_states=output_hidden_states,
+    return_dict=return_dict,
+    inputs_embeds=input_embeddings,  
+    labels=labels)
+    
+    def generate(self, input_ids, attention_mask, max_length=50, num_beams = 5, early_stopping=True):
+        input_embeddings = self.shared(input_ids)  # Convert input IDs to embeddings
+
+        #Map input embeddings to hyperbolic space
+        hyperbolic_input_embeddings = expmap0(input_embeddings, self.curvature)
+
+        outputs = self.generate(inputs_embeds=hyperbolic_input_embeddings, attention_mask=attention_mask, max_length=max_length, num_beams=num_beams, early_stopping=early_stopping)
         return outputs
     

src/train/soft_prompt_trainer.py
@@ -101,13 +101,9 @@ class SoftPromptTrainer:
                 inputs = self.tokenizer(input_batch, padding=True, truncation=True, return_tensors = 'pt').to(self.device)
                 labels = self.tokenizer(label_batch, padding=True, truncation=True, return_tensors = 'pt')['input_ids'].to(self.device)
                 
-                outputs = self.model(inputs, labels=labels)
-                for name, param in self.model.named_parameters():
-                    print(f"Parameter {name} requires_grad: {param.requires_grad}")              
+                outputs = self.model(inputs, labels=labels)       
                 loss = outputs.loss
-                #print(f"Loss requires_grad: {loss.requires_grad}") 
-                with torch.autograd.set_detect_anomaly(True):
-                    loss.backward()
+                loss.backward()
 		        
                 self.optimizer.step()
 